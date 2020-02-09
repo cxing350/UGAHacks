50f40f7286c412bc833a66b81b8a655ebb6dc786
Corey Xing <corey.xing@gatech.edu>
2020-02-09 02:31:51 -0500

    added displays

--- sunflowers/sunflowers.ino
+++ sunflowers/sunflowers.ino
@@ -3,6 +3,15 @@
 #include <DFRobot_BMP388.h>
 #include <DFRobot_BMP388_I2C.h>
 #include <DFRobot_BMP388_SPI.h>
+#include <Wire.h> // Include the Arduino SPI library
+#include <rgb_lcd.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+const int red = 255;
+const int green = 0;
+const int blue = 255;
+
 
 DFRobot_BMP388_I2C bmp388;
 
@@ -11,8 +20,61 @@ int led2 = 12;
 int count = 0;
 Servo myServo;
 
+const byte s7sAddress = 0x71;
+
+byte heart[8] = {
+    0b00000,
+    0b01010,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b01110,
+    0b00100,
+    0b00000
+};
+
+byte smiley[8] = {
+    0b00000,
+    0b00000,
+    0b01010,
+    0b00000,
+    0b00000,
+    0b10001,
+    0b01110,
+    0b00000
+};
+
+byte mongo[8] = {
+    0b00100,
+    0b01010,
+    0b00100,
+    0b10101,
+    0b01110,
+    0b00100,
+    0b00100,
+    0b01010
+};
+
 // the setup routine runs once when you press reset:
 void setup() {
+  //lcd setup
+  Wire.begin();
+  
+
+  //rgb
+  lcd.begin(16, 2);
+  lcd.setRGB(red, green, blue);
+  lcd.print("Thank You Mongo!");
+  lcd.createChar(0, heart);
+  lcd.createChar(1, smiley);
+  lcd.createChar(2, mongo);
+  lcd.setCursor(4, 1);
+  lcd.write((unsigned char) 0);
+  lcd.setCursor(8, 1);
+  lcd.write((unsigned char) 1);
+  lcd.setCursor(12, 1);
+  lcd.write(2);
+   
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   pinMode(led1, OUTPUT);
@@ -49,6 +111,23 @@ void loop() {
   }
 
   int currentAngle = myServo.read();
+  String send = String(currentAngle);
+  Wire.beginTransmission(s7sAddress);
+  if (send.length() > 2) {
+    Wire.write("0");
+    for (int i = 0; i < 3; i++) {
+      Wire.write(send[i]);
+    }
+  } else if (send.length() == 2) {
+    Wire.write("00");
+    for (int i = 0; i < 2; i++) {
+      Wire.write(send[i]);
+    }
+  } else if (send.length() == 1) {
+    Wire.write("000");
+    Wire.write(send[0]);
+  }
+  Wire.endTransmission();
 
   if (avg1 > avg2) {
     greater = avg1;
@@ -76,4 +155,5 @@ void loop() {
     count = 0;
   }
   count++;
+
 }
